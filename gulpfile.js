c99a0b26e61b39f4c6c285e9f5a8457b4c8112c2
nilptr <nilptr.js@gmail.com>
2016-02-24 23:49:20 +0800

    页面基本完成

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 gulp.task('sass', function () {
   return gulp.src('src/assets/scss/*.scss')
@@ -10,6 +12,15 @@ gulp.task('sass', function () {
 gulp.task('copy', function () {
   return gulp.src('src/**/*.{html,jpg}')
     .pipe(gulp.dest('build'));
+});
+
+gulp.task('prism', function () {
+  return gulp.src([
+    'bower_components/prism/prism.js',
+    'bower_components/prism/components/*.min.js'
+  ]).pipe(concat('prism.pack.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('build/assets/js'));
 });
 
 gulp.task('default', ['sass', 'copy']);
